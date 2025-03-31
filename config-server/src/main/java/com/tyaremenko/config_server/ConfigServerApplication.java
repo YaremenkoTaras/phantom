8690c9ac68ec8ca5ee3ae1b5aca02410f2493b41
Taras Yaremenko <tyaremenko@playtika.com>
2025-03-31 18:23:14 +0300

    Add config server

---
+++ config-server/src/main/java/com/tyaremenko/config_server/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.tyaremenko.config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
